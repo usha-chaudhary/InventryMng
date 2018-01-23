c51ccbf9f8f9e275a48d2c63f456da31724e92b7
upashanaupa <upashanaupa12@gmail.com>
2018-01-23 07:36:54 +0545

    change

--- src/main/java/com/bim5ths/demo/controller/ProductController.java
+++ src/main/java/com/bim5ths/demo/controller/ProductController.java
@@ -1,8 +1,7 @@
 package com.bim5ths.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.bim5ths.demo.dao.ProductDAO;
-import com.bim5ths.demo.entity.Customer;
-import com.bim5ths.demo.entity.OrderIteam;
 import com.bim5ths.demo.entity.Product;
-import com.bim5ths.demo.service.CustomerService;
 import com.bim5ths.demo.service.ProductService;
 
 @RestController
